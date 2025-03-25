97de39fd688fb24d59fe94d9855d067812c16f73
Mercedes Garcia Morales <mergarci@c2r16s1.42madrid.com>
2025-03-25 17:47:36 +0100

    20230325: starting to read argv

--- src/inputs.c
+++ src/inputs.c
@@ -6,20 +6,30 @@
 /*   By: mergarci <mergarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 20:42:59 by mergarci          #+#    #+#             */
-/*   Updated: 2025/03/23 21:00:50 by mergarci         ###   ########.fr       */
+/*   Updated: 2025/03/25 17:45:39 by mergarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int check_command(char *command)
+char *check_command(char *command)
 {
+    char *path;
     
+    path = ft_strjoin("/bin/", command);
+    printf("%s\n", path);
+    if (access(path, X_OK) < 0)
+    {
+        free(path);
+        path = NULL;
+    }
+    return (path);
 }
 
-int check_argv(int argc, char *argv[])
+char **check_argv(int argc, char *argv[])
 {
     int i;
+    char **out;
     char *file_in;
     char *file_out;
     char *command1;
@@ -27,9 +37,23 @@ int check_argv(int argc, char *argv[])
     
     if (argc >= 5)
     {
-        file_in = argv[1];
-        file_out = argv[argc - 1];
-        command1 = argv[2];
-        command2 = argv[3];
+        out = ft_calloc(argc - 1, sizeof(char *));
+        while (i <= argc)
+        {
+            if (i == 0) // primer fichero
+            {}
+            else if (i == argc - 1) //ultimo fichero
+            {}
+            else
+            {
+                out[i] = check_command(argv[i + 1]);
+            }
+            file_in = argv[1];
+            file_out = argv[argc - 1];
+            i++;
+            command1 = check_command(argv[1]);
+            command2 = check_command(argv[2]);
+        }
     }
+    return i;
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mergarci <mergarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 20:05:13 by mergarci          #+#    #+#             */
-/*   Updated: 2025/03/23 21:05:10 by mergarci         ###   ########.fr       */
+/*   Updated: 2025/03/25 17:25:33 by mergarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 
 int main(int argc, char *argv[])
 {
-    int err; 
+    /*int err; 
     int fd = access("/bin/", F_OK);
     if (fd == -1) {
         printf("Error Number: %d\n", errno);
         perror("Error Description:");
     } else {
         printf("No error\n");
-    }
-    
+    }*/
+    const char *path = "/bin/wc";
+    char *const command[] = { "/bin/wc", "-a", NULL };
+    char *const envp[] = { "PATH=/bin", NULL };
+
+    //execve(path, command, envp);
+    check_argv(argc, argv);
+
+    // Si execve tiene éxito, este código no se alcanza
+    perror("execve");  // Solo se ejecuta en caso de error
     return (0);
 }
