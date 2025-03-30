b9140d9ad20c2e0522079074009f0a8b5153e3f3
Merche_Linux_Asus <mergarciam@gmail.com>
2025-03-31 00:00:01 +0200

    20250330: added pipe function and execve function

--- src/main.c
+++ src/main.c
@@ -6,26 +6,77 @@
 /*   By: mergarci <mergarci@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 20:05:13 by mergarci          #+#    #+#             */
-/*   Updated: 2025/03/28 20:24:10 by mergarci         ###   ########.fr       */
+/*   Updated: 2025/03/30 23:58:42 by mergarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+int	ft_child(int *fd, char *argv[])
+{
+	char *path = "/bin/cat";
+	char *args[] = {"/bin/cat", NULL};
+    char *envp[] = {"PATH=/bin/", NULL};
+	
+	int fd_file = open("src/main.c", O_RDONLY);
+	if (fd_file > 0)
+	{
+		printf("primer hijo: %d\n", getpid());
+		dup2(fd_file, STDIN_FILENO);
+		//close(fd[READ]); /*cerramos extremo no necesario*/
+		dup2(fd[WRITE], STDOUT_FILENO);
+		close(fd[WRITE]);
+		execve(path,args, envp);
+		//execlp("/bin/cat", "cat", NULL);
+		close(fd_file);
+		exit (0);
+	}
+	else
+		ft_printf("error abriendo fichero de entrada\n");
+}
 int main(int argc, char *argv[])
 {
-    char **inputs;
-    const char *path = "/bin/wc";
-    char *const command[] = { "/bin/wc", "-a", NULL };
-    char *const envp[] = { "PATH=/bin", NULL };
+	int fd[2];
+	int fd_file;
+	pid_t pid;
+	int status;
+	char buf[50];
+	int num_bytes;
 
-    //execve(path, command, envp);
-    inputs = check_argv(argc, argv);
-	if (inputs)
-		printf("todo ok!\n");
-	else
-		printf("caca\n");
-    // Si execve tiene éxito, este código no se alcanza
-    perror("execve");  // Solo se ejecuta en caso de error
-    return (0);
+	char *path = "/bin/wc";
+	char *args[] = {"/bin/wc", "-l",NULL};
+    char *envp[] = {"PATH=/bin/", NULL};
+	
+	if (argc == 5)
+	{
+		if (pipe(fd) != 0)
+			return (1);
+		pid = fork();
+		if (pid == 0)  //primer hijo. El que ejecuta ls
+		{
+			ft_child(fd, argv);
+		}
+		else //estamos en el padre
+		{
+			waitpid(pid, &status, 0);
+			printf("padre: %d\n", getpid());
+			close(fd[WRITE]); /*cerramos extremo no necesario*/
+			/*num_bytes = read(fd[READ], buf, sizeof(buf));
+			printf("%d: %s\n", num_bytes, buf);*/
+			fd_file = open(FILE_NAME, O_WRONLY);
+			printf("fd2: %d\n", fd_file);
+			if (fd_file > 0)
+			{
+				dup2(fd[READ], STDIN_FILENO);
+				close(fd[READ]);
+				dup2(fd_file, STDOUT_FILENO);
+				close(fd_file);
+				execve(path,args, envp);
+			}
+			close(fd[READ]);
+		}
+	}
+	else 
+		ft_printf("ERROR: Invalid inputs. Please introduce:\n./pipex infile command1 command2 outfile\n");
+	return (0);
 }
--- src/pipex.h
+++ src/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mergarci <mergarci@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 20:05:31 by mergarci          #+#    #+#             */
-/*   Updated: 2025/03/28 20:22:42 by mergarci         ###   ########.fr       */
+/*   Updated: 2025/03/29 14:22:06 by mergarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,18 @@
 # include "../libft/ft_printf.h"
 # include <unistd.h>
 # include <stdio.h>
-
+#include <sys/wait.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define READ 0 /*extremo lectura*/
+#define WRITE 1 /*extremo escritura*/
+
+#define FILE_NAME "file.txt"
 
 //inputs.c
 char  **check_argv(int argc, char *argv[]);
