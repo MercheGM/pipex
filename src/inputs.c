eb7f1a47f3f67229b4196101f078caf254b056c8
Merche_Linux_Asus <mergarciam@gmail.com>
2025-04-13 21:27:20 +0200

    20250413: fixed enviroment issue from execve

--- src/inputs.c
+++ src/inputs.c
@@ -6,23 +6,11 @@
 /*   By: mergarci <mergarci@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 20:42:59 by mergarci          #+#    #+#             */
-/*   Updated: 2025/04/12 11:35:36 by mergarci         ###   ########.fr       */
+/*   Updated: 2025/04/13 21:12:01 by mergarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-/*Function to fill info with the enviroment info and the PATH variable*/
-static char	**ft_fill_envp(char **envp)
-{
-	char	**aux;
-
-	aux = ft_calloc(3, sizeof(char *));
-	aux[0] = "PATH=/usr/bin:/bin";
-	aux[1] = envp[0];
-	aux[2] = NULL;
-	return (aux);
-}
 
 /*Checks the command and excecutes it. Funtion checks first if the command
 exits and then it splits the info from the command and gets the enviroment
@@ -31,7 +19,6 @@ int	check_command(char *command, char **envp)
 {
 	char	*path;
 	char	**args;
-	char	**envp_final;
 
 	args = ft_split(command, ' ');
 	if (args == NULL)
@@ -43,8 +30,7 @@ int	check_command(char *command, char **envp)
 		perror("access");
 		exit (errno);
 	}
-	envp_final = ft_fill_envp(envp);
-	if (execve(path, args, envp_final) == -1)
+	if (execve(path, args, envp) == -1)
 	{
 		perror("execve");
 		exit(errno);
